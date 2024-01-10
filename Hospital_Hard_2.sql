7146761450e49d249555898c39f0bc6c6f9cb02f
Sayantani Dey <88243330+musicallysouled@users.noreply.github.com>
2024-01-10 23:52:21 +0530

    Update Hospital_Hard_2.sql

--- Hospital_Hard_2.sql
+++ Hospital_Hard_2.sql
@@ -1,3 +1,4 @@
+/*
 Show patient_id, weight, height, isObese from the patients table.
 Display isObese as a boolean 0 or 1.
 
@@ -5,6 +6,7 @@ Obese is defined as weight(kg)/(height(m)2) >= 30.
 weight is in units kg.
 height is in units cm.
 --------------------------------------------------------------------------------------------------------------------------
+*/
 SELECT patient_id, weight, height, 
   (CASE 
       WHEN weight/(POWER(height/100.0,2)) >= 30 THEN
