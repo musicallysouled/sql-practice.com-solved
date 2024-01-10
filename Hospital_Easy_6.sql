1e2c6468fadae54137f859424636fd9c93845bca
Sayantani Dey <88243330+musicallysouled@users.noreply.github.com>
2024-01-10 23:49:13 +0530

    Update Hospital_Easy_6.sql

--- Hospital_Easy_6.sql
+++ Hospital_Easy_6.sql
@@ -1,5 +1,7 @@
-/*Show first name and last name concatinated into one column to show their full name.
-/*------------------------------------------------------------------------------
+/*
+Show first name and last name concatinated into one column to show their full name.
+------------------------------------------------------------------------------
+*/
 select 
-concat(first_name,' ', last_name) as full_name
+  concat(first_name,' ', last_name) as full_name
 from patients;
