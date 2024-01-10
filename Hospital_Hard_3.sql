b13368c5869fc2c1d9487ec422e3cd56d0668129
Sayantani Dey <88243330+musicallysouled@users.noreply.github.com>
2024-01-10 23:52:57 +0530

    Update Hospital_Hard_3.sql

--- Hospital_Hard_3.sql
+++ Hospital_Hard_3.sql
@@ -1,10 +1,12 @@
+/*
 Show patient_id, first_name, last_name, and attending doctor's specialty.
 Show only the patients who has a diagnosis as 'Epilepsy' and the doctor's first name is 'Lisa'
 
 Check patients, admissions, and doctors tables for required information.
 -----------------------------------------------------------------------------------------------
+*/
 SELECT p.patient_id as patient_id,
-	   p.first_name as patient_first_name,
+       p.first_name as patient_first_name,
        p.last_name as patient_last_name,
        d.specialty as attending_doctor_specialty
 FROM patients p
