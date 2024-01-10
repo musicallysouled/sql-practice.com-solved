4eb49cd5bb6729b4e59861214a5fa6068febdd4b
Sayantani Dey <88243330+musicallysouled@users.noreply.github.com>
2024-01-10 23:50:15 +0530

    Update Hospital_Easy_2.sql

--- Hospital_Easy_2.sql
+++ Hospital_Easy_2.sql
@@ -1,4 +1,5 @@
-/*Show first name and last name of patients who does not have allergies. (null)
+/*
+Show first name and last name of patients who does not have allergies. (null)
 ----------------------------------------------------------------------------------------------------------
 */
 SELECT first_name, last_name
