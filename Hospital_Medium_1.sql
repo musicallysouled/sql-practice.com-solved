70684d865c70edab3dab78e35dd044eea3c0e079
Sayantani Dey <88243330+musicallysouled@users.noreply.github.com>
2024-01-10 23:53:25 +0530

    Update Hospital_Medium_1.sql

--- Hospital_Medium_1.sql
+++ Hospital_Medium_1.sql
@@ -1,5 +1,7 @@
+/*
 Show unique birth years from patients and order them by ascending.
 -------------------------------------------------------------------------------------------
+*/
 SELECT distinct year(birth_date) as birth_years
 FROM patients
 order by birth_years;
