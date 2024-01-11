f2c0f01e84c05139e477adfc4db4ce0665d0ef8c
Sayantani Dey <88243330+musicallysouled@users.noreply.github.com>
2024-01-11 13:40:27 +0530

    Create Hospital_Hard_5.sql

---
+++ Hospital_Hard_5.sql
@@ -0,0 +1,14 @@
+/*
+Each admission costs $50 for patients without insurance, and $10 for patients with insurance. All patients with an even patient_id have insurance.
+Give each patient a 'Yes' if they have insurance, and a 'No' if they don't have insurance. Add up the admission_total cost for each has_insurance group.
+-------------------------------------------------------------------------------------------------------------------
+*/
+select has_insurance,sum(admission_cost) as admission_total
+from
+(
+   select patient_id,
+   case when patient_id % 2 = 0 then 'Yes' else 'No' end as has_insurance,
+   case when patient_id % 2 = 0 then 10 else 50 end as admission_cost
+   from admissions
+)
+group by has_insurance
