9033a22d5d3e903a0ceb33a1dd1162ef06affbc8
Sayantani Dey <sayantanidey07@outlook.com>
2024-02-24 19:22:25 +0530

    Update Hospital_Hard_10.sql

--- Hospital_Hard_10.sql
+++ Hospital_Hard_10.sql
@@ -4,4 +4,4 @@ Show all columns for patients who have one of the following patient_ids:
 */
 SELECT *
 FROM patients
-where patient_id in (1, 45 , 534, 879, 1000);
+where patient_id in (1, 45 , 534, 879, 1000) ;
